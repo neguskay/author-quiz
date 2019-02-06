dc6cafd5c96c25d24f4b8605f4cb180360f9983e
Sidney Ofosu-Ottopah <sofosu-ottopah@MacBook-Pro-40.local>
2019-02-06 18:38:55 +0000

    First React App

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,104 @@
+//eslint-disable-next-line
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+//import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+
+function Book({title, onClick}){
+  return(
+    <div className="answer" onClick={() =>{onClick(title);}}>
+      <h4>
+        {title}
+      </h4>
+    </div>
+    );
+}
+
+function Hero() {
+  return(
+    <div className = "row">
+      <div className = "jumbotro col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select The Book Written By The Author Shown</p>
+      </div>
+    </div>    
+  );
+}
+
+function Turn({author, books, highlight, onAnswerSelected}){
+
+  function highlighToBgColour(highlight){
+    const mapping = {
+      "none": "",
+      "correct": "green",
+      "wrong": "red"
+    };
+
+    return mapping[highlight];
+  }
+
+  return(
+    <div className="row turn" style={{backgroundColor: highlighToBgColour(highlight)}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author"/>
+
+      </div>
+      <div className="col-6">
+        {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected}/>)}
+      </div>
+    </div>
+  );
+}
+
+Turn.propTypes = {
+  author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+      imageSource: PropTypes.string.isRequired,
+      books: PropTypes.arrayOf(PropTypes.string).isRequired
+
+  }),
+
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
+
+function Continue(){
+  return(
+    <div>
+      
+    </div>
+  );
+}
+function AuthorQuiz({turnData, highlight, onAnswerSelected}){
+  return (
+    <div className="container-fluid">    
+      <Hero/>
+      <Turn{...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
+      <Continue/> 
+    </div>
+  );
+}
+
+export default AuthorQuiz;
+
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
